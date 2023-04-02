9d7283cd259d6a46c9f32735eb9544bd4dbb3b2c
sad ass <102435521+InnerShadow@users.noreply.github.com>
2023-04-02 17:12:48 +0300

    Test MPI

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+#include "mpi.h"
+
+int main(int argc, char** argv) {
+    int rank, size;
+    int message = 42;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (rank == 1) {
+        MPI_Recv(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        std::cout << "Received message: " << message << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
